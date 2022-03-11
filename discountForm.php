6e9830af2f467efed3f19a2a474248b4cd7b13ad
YoshihitoIshida <90079388+YoshihitoIshida@users.noreply.github.com>
2022-03-11 23:09:26 +0900

    Update discountForm.php
    Use coupon codes and product IDs

--- discountForm.php
+++ discountForm.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>8-4</title>
+<title>8-5 P298</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -27,18 +27,28 @@ if (isset($_POST['kosu'])) {
 ?>
 
 <?php
-$discount = 0.8;
+require_once("saledata.php");
+$couponCode = "ha45as";
+$goodsID = "ax102";
+$discount = getCouponRate($couponCode);
+$tanka = getPrice($goodsID);
+if(is_null($discount)||is_null($tanka)){
+  $err = '<div class="error">不正な操作がありました。</div>';
+  exit($err);
+}
+?>
+<?php
 $off = (1-$discount)*100;
   if ($discount>0) {
     echo "<h2>このページでの購入は{$off}% OFFになります！</h2>";
   }
-$tanka = 2900;
 $tanka_fmt = number_format($tanka);
 ?>
   <!__入力フォームを作る__>
 <form method="POST" action="discount.php">
-  <input type="hidden" name="discount" value="<?php echo $discount; ?>">
-  <input type="hidden" name="tanka" value="<?php echo $tanka; ?>">
+  <!__隠しフィールドにクーポンコードと商品IDを設定してPOSTする__ >
+  <input type="hidden" name="couponCode" value="<?php echo $couponCode; ?>">
+  <input type="hidden" name="goodsID" value="<?php echo $goodsID; ?>">
   <ul>
     <li><label>単価：<?php echo $tanka_fmt; ?>円</label></li>
     <li><label>個数：<input type="number" name="kosu" value="<?php echo $kosu; ?>"></label></li>
